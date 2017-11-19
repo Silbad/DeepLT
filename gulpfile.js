b8c198a4fb2df362bbd052cf987390ef0a3f904d
silbad <psilbad@gmail.com>
2017-11-19 23:45:57 +0100

    Add localization, Gulp script & minor improvements

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,71 @@
 'use strict';
- 
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var rename = require('gulp-rename');
- 
-gulp.task('sass', function () {
-  return gulp.src('./scss/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-	.pipe(rename('deeplt.css'))
-    .pipe(gulp.dest('./css'));
-});
- 
-gulp.task('watch', function () {
-  gulp.watch('./scss/**/*.scss', ['sass']);
+var syncy = require('syncy');
+let cleanCSS = require('gulp-clean-css');
+
+// mise à jour des fichiers spécifiques provenant de node_modules
+gulp.task('sync-jquery', (done) => {
+    syncy(['node_modules/jquery/dist/*.js'], 'js/', {
+        verbose: true,
+        updateAndDelete: false,
+        base: 'node_modules/jquery/dist/'
+    })
+    .then(() => {
+        done();
+    })
+    .catch((err) => {
+        done(err);
+    });
 });
 
-gulp.task('default', ['sass', 'watch']);
+gulp.task('sync-popper', (done) => {
+    syncy(['node_modules/popper.js/dist/umd/*.js'], 'js/', {
+        verbose: true,
+        updateAndDelete: false,
+        base: 'node_modules/popper.js/dist/umd/'
+    })
+    .then(() => {
+        done();
+    })
+    .catch((err) => {
+        done(err);
+    });
+});
+
+gulp.task('sync-bootstrap', (done) => {
+    syncy(['node_modules/bootstrap/dist/js/*.js'], 'js/', {
+        verbose: true,
+        updateAndDelete: false,
+        base: 'node_modules/bootstrap/dist/js/'
+    })
+    .then(() => {
+        done();
+    })
+    .catch((err) => {
+        done(err);
+    });
+});
+
+// sass -> css -> min
+gulp.task('sass', function () {
+    return gulp.src('./scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(rename('deeplt.css'))
+        .pipe(gulp.dest('./css'))
+        .on('end', function () {
+            return gulp.src('./css/*.css')
+                .pipe(cleanCSS())
+                .pipe(rename('deeplt-min.css'))
+                .pipe(gulp.dest('./css'));
+        });
+});
+
+// watch
+gulp.task('watch', function () {
+    gulp.watch('scss/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sync-jquery', 'sync-popper', 'sync-bootstrap', 'sass', 'watch']);
--- js/deeplt.js
+++ js/deeplt.js
@@ -1,7 +1,35 @@
 $(function() {
-    $('#trad-search').on('keyup', function() {
-        var text = $(this).val().trim();
-		if (text == '') {
+    // add localization i18n items
+    var name = browser.i18n.getMessage('deepLTName');
+    var description = browser.i18n.getMessage('deepLTDescription');
+    var auto = browser.i18n.getMessage('deepLTAuto');
+    var french = browser.i18n.getMessage('deepLTFrench');
+    var english = browser.i18n.getMessage('deepLTEnglish');
+    var german = browser.i18n.getMessage('deepLTGerman');
+    var spanish = browser.i18n.getMessage('deepLTSpanish');
+    var italian = browser.i18n.getMessage('deepLTItalian');
+    var dutch = browser.i18n.getMessage('deepLTDutch');
+    var polish = browser.i18n.getMessage('deepLTPolish');
+    var textToBeTranslated = browser.i18n.getMessage('deepLTTextToBeTranslated');
+
+    $('#lang-origin option[value="auto"]').html(auto);
+    $('#lang-origin option[value="FR"], #lang-target option[value="FR"]').html(french);
+    $('#lang-origin option[value="EN"], #lang-target option[value="EN"]').html(english);
+    $('#lang-origin option[value="DE"], #lang-target option[value="DE"]').html(german);
+    $('#lang-origin option[value="ES"], #lang-target option[value="ES"]').html(spanish);
+    $('#lang-origin option[value="IT"], #lang-target option[value="IT"]').html(italian);
+    $('#lang-origin option[value="NL"], #lang-target option[value="NL"]').html(dutch);
+    $('#lang-origin option[value="PL"], #lang-target option[value="PL"]').html(polish);
+    $('#trad-search').attr('placeholder', textToBeTranslated).attr('aria-label', textToBeTranslated);
+
+    // add event translation system
+    $('#trad-search, #lang-origin, #lang-target').on('keyup change', function() {
+
+        var text = $('#trad-search').val().trim();
+        var lang_origin = $('#lang-origin').val();
+        var lang_target = $('#lang-target').val();
+
+		if (text.length < 3) {
 			$('#trad-result').val('');
 		}
 		else {
@@ -21,8 +49,8 @@ $(function() {
 						}],
 						lang: {
 							user_preferred_langs: ['FR','EN'],
-							source_lang_user_selected: 'FR',
-							target_lang: 'EN'
+							source_lang_user_selected: lang_origin,
+							target_lang: lang_target
 						},
 						priority: -1
 					},
@@ -34,7 +62,7 @@ $(function() {
 				error:function(xhr,status,error){
 					console.log(xhr,status,error);
 				}
-			});		
+			});
 		}
     });
 });
